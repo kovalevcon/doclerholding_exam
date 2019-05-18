bd72f98100deb7c4fb9c5a79eb31581b1b50d4bc
kovalevcon <kovalevcon@mail.ru>
2019-05-19 00:20:58 +0600

    Formating fix, adding phpdoc in mock classes

--- src/Config.php
+++ src/Config.php
@@ -1,22 +1,31 @@
 <?php
-
 namespace Language;
 
+/**
+ * Class work with configuration
+ *
+ * Class Config
+ * @package Language
+ */
 class Config
 {
-	public static function get($key)
-	{
-		switch ($key)
-		{
-			case 'system.paths.root' :
-				return realpath(dirname(__FILE__) . '/../');
-				break;
+    /**
+     * Get config path or parameters
+     *
+     * @param string $key
+     * @return array|bool|string|void
+     */
+    public static function get(string $key)
+    {
+        switch ($key) {
+            case 'system.paths.root':
+                return realpath(dirname(__FILE__) . '/../');
 
-			case 'system.translated_applications':
-				return ['portal' => ['en', 'hu']];
+            case 'system.translated_applications':
+                return ['portal' => ['en', 'hu']];
 
-			default:
-				return;
-		}
-	}
+            default:
+                return;
+        }
+    }
 }
